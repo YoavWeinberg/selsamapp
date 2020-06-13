80ef078104a2e68b3f0cc44392b37ee77ee005b8
YoavWeinberg <yoav.wain7@gmail.com>
2020-06-13 16:18:47 +0300

    Do It Better

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app=express();
 app.get('/',function(req, res, next){
-res.json({FirstMessage:"Unnesesery Text...."});
+res.json({SumNumbersOfPy :"3.141592653589793238462643383279502884197169399375105"});
 res.json({SumNumbersOfPy :"3.141592653589793238462643383279502884197169399375105"});
 });
 var port = process.env.PORT || 3000;
