34336a27fd402c06b3c624659b8bc56efe3d896c
Weinberg Yoav <yoav.wain7@gmail.com>
2020-08-14 16:53:18 +0300

    make it better

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var app=express();
 var bodyParser = require('body-parser');
 var api = require('./api/api');
 
+app.get('/',function(req, res, next){
+    console.log("The Site was opend")
+    res.json({Message :"This Is My New Site"});
+});
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/api', api);
 
-var port = process.envPort || 5555;
+var port = process.envPort || 5353;
 app.listen(port, function(){
     console.log("Runnig on port " + port);
 });
