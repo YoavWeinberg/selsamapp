2ef6c711961707f95a2f720d31950f43a2fa15f7
Weinberg Yoav <yoav.wain7@gmail.com>
2020-08-14 16:45:20 +0300

    make it better

--- api/api.js
+++ api/api.js
@@ -1,7 +1,5 @@
 var router = require('express').Router();
 router.use('/product', require('./Product/ProductsRoutes'));
 router.use('/post', require('./Blog/PostRoutes'));
-router.use('/user', require('./User/UserRoutes'));
 
-router.use('/auth', require('../auth/routes'));
 module.exports = router;
